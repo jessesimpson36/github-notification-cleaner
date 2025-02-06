818cfb3403d43611700d1e393acbaee9659ddb28
Jesse Simpson <jesse.simpson36@gmail.com>
2025-02-06 15:26:37 +0100

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/jessesimpson36/github-notification-cleaner
+
+go 1.22.1
+
+require github.com/google/go-github/v69 v69.0.1-0.20250206001409-24b0795da596
+
+require github.com/google/go-querystring v1.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/go-github/v69/github"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func printInfo(notification *github.Notification, pr *github.PullRequest, prNumberStr string, url string) {
+
+	fmt.Println("Number: " + prNumberStr)
+	fmt.Println("Url: " + url)
+	fmt.Println("Subject: " + *notification.Subject.Title)
+	fmt.Println("Reason: " + *notification.Reason)
+	fmt.Println("state: " + *pr.State)
+}
+
+func main() {
+	client := github.NewClient(nil).WithAuthToken(os.Getenv("GITHUB_TOKEN"))
+	ctx := context.Background()
+	opt := &github.NotificationListOptions{}
+	notifications, response, err := client.Activity.ListNotifications(ctx, opt)
+	if err != nil {
+		fmt.Println("error " + err.Error() + " " + response.Status)
+		os.Exit(1)
+	}
+	for _, notification := range notifications {
+		url := *notification.Subject.URL
+		urlBytes := []byte(url)
+		matched, err := regexp.Match(`pulls/[0-9]*`, []byte(url))
+		if err != nil {
+			continue
+		}
+		if !matched {
+			continue
+		}
+		pullsNumRe := regexp.MustCompile(`pulls/[0-9]+`)
+		foundB := pullsNumRe.Find(urlBytes)
+		numRe := regexp.MustCompile(`[0-9]+`)
+		prNumber := numRe.Find(foundB)
+		prNumberStr := string(prNumber)
+		prNumberInt, err := strconv.Atoi(prNumberStr)
+		if err != nil {
+			fmt.Println("error " + err.Error())
+			os.Exit(1)
+		}
+
+		pr, _, err := client.PullRequests.Get(ctx, *notification.Repository.Owner.Login, *notification.Repository.Name, prNumberInt)
+		if err != nil {
+			fmt.Println("error " + err.Error())
+			os.Exit(1)
+		}
+		if *pr.State == "closed" {
+			printInfo(notification, pr, prNumberStr, url)
+			idInt, err := strconv.Atoi(*notification.ID)
+			if err != nil {
+				fmt.Println("error " + err.Error())
+				os.Exit(1)
+			}
+			resp, err := client.Activity.MarkThreadDone(ctx, int64(idInt))
+			if err != nil {
+				fmt.Println("error " + err.Error() + " " + resp.Status)
+				os.Exit(1)
+			}
+			fmt.Println("Marked as done")
+			fmt.Println()
+		}
+
+	}
+}
